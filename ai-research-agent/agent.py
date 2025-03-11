20d3144fb8cb5c145daf2ef83eab20a700355a3d
Robbie Heywood <robbie@portialabs.ai>
2025-03-11 19:50:53 +0000

    Add CI check

--- ai-research-agent/agent.py
+++ ai-research-agent/agent.py
@@ -108,14 +108,16 @@ with execution_context(
     # An alternative would be to just call portia.run() which will do both.
     plan = portia.plan(
         "Read all emails from today that contain 'AI' and summarise them into a single, coherent summary (i.e. don't summarise each email separately). "
-        "Then post the summary with links to the #ai-news channel."
+        "Then post the summary with links to the #app-testing channel."
         "Then, create a short podcast script based on the emails, driven by the summary but with further details coming from the emails."
     )
     print("\nHere are the steps in the generated plan:")
     [print(step.model_dump_json(indent=2)) for step in plan.steps]
-    user_input = input("Are you happy with the plan? (y/n):\n")
-    if user_input != "y":
-        sys.exit(1)
+
+    if os.getenv("CI") != "true":
+        user_input = input("Are you happy with the plan? (y/n):\n")
+        if user_input != "y":
+            sys.exit(1)
 
     run = portia.run_plan(plan)
 
