884668c84d03d8200af86958edf91aad27496664
robbie-portia <robbie@portialabs.ai>
2025-03-12 17:51:36 +0000

    Fix (#14)

--- ai-research-agent/agent.py
+++ ai-research-agent/agent.py
@@ -37,7 +37,7 @@ class PodcastToolSchema(BaseModel):
 
 
 class PodcastTool(Tool[str]):
-    """Create a podcast script based on a high-level summary and further details."""
+    """Create a podcast based on a high-level summary and further details."""
 
     id: str = "podcast_tool"
     name: str = "Podcast Tool"
@@ -108,8 +108,8 @@ with execution_context(
     # An alternative would be to just call portia.run() which will do both.
     plan = portia.plan(
         "Read all emails from today that contain 'AI' and summarise them into a single, coherent summary (i.e. don't summarise each email separately). "
-        "Then post the summary with links to the #ai-news channel."
-        "Then, create a short podcast script based on the emails, driven by the summary but with further details coming from the emails."
+        "Then post the summary with links to the #app-testing channel."
+        "Then, create a short podcast based on the emails, driven by the summary but with further details coming from the emails."
     )
     print("\nHere are the steps in the generated plan:")
     [print(step.model_dump_json(indent=2)) for step in plan.steps]
