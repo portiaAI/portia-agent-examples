b68b26ffc89b4577cf99369b957e282d3318d39d
Mark Smith <judy@judy.co.uk>
2025-04-16 18:19:13 +0100

    Fix config loading so it happens before we use it.

--- improving-planning-with-ull/common.py
+++ improving-planning-with-ull/common.py
@@ -120,34 +120,33 @@ class RefundReviewerTool(Tool[str]):
         return False
 
 
-config = Config.from_default(default_log_level="INFO")
-
-tools = (
-    McpToolRegistry.from_stdio_connection(
-        server_name="stripe",
-        command="npx",
-        args=[
-            "-y",
-            "@stripe/mcp",
-            "--tools=all",
-            f"--api-key={os.environ['STRIPE_TEST_API_KEY']}",
-        ],
-    )
-    + DefaultToolRegistry(
-        config=config,
-    )
-    + InMemoryToolRegistry.from_local_tools(
-        [RefundReviewerTool(), RefundHumanApprovalTool()]
-    )
-)
-
-
 def init_portia():
     """
     Load config from a `.env` file and return a configured instance of `Portia`.
     """
     
     load_dotenv(override=True)
+
+    config = Config.from_default(default_log_level="INFO")
+
+    tools = (
+        McpToolRegistry.from_stdio_connection(
+            server_name="stripe",
+            command="npx",
+            args=[
+                "-y",
+                "@stripe/mcp",
+                "--tools=all",
+                f"--api-key={os.environ['STRIPE_TEST_API_KEY']}",
+            ],
+        )
+        + DefaultToolRegistry(
+            config=config,
+        )
+        + InMemoryToolRegistry.from_local_tools(
+            [RefundReviewerTool(), RefundHumanApprovalTool()]
+        )
+    )
 
     portia_instance = Portia(
         config=config, tools=tools, execution_hooks=CLIExecutionHooks()
