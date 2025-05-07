a90e77db1dd84e04ff84ca8a3b2f65d984cd7d35
Mark Smith <judy@judy.co.uk>
2025-05-07 12:54:13 +0100

    Added argparse to main method and minor re-org of main code.

--- local-llm/main.py
+++ local-llm/main.py
@@ -1,4 +1,8 @@
+import argparse
 import os
+import sys
+
+import portia.tool
 from dotenv import load_dotenv
 from portia import (
     Config,
@@ -8,21 +12,35 @@ from portia import (
     Portia,
     ToolRegistry,
 )
-from portia.plan import PlanBuilder
 from portia.cli import CLIExecutionHooks
-import portia.tool
+from portia.plan import PlanBuilder
+
 from tools.visualization_tool import VisualizationTool
 
 portia.tool.MAX_TOOL_DESCRIPTION_LENGTH = 2048
 
+
 def create_plan_local(portia: Portia, note_name: str):
-    plan = PlanBuilder(f"Create a concept map image from the note with title {note_name}") \
-        .step("List all available vaults", "mcp:obsidian:list_available_vaults") \
-        .step(f"Fetch the note named '{note_name}' from the obsidian vaults", "mcp:obsidian:read_note") \
-        .step(f"Create a concept map visualization using the extracted relationships. Title the image {note_name} and output the image to the directory {os.getenv("OBSIDIAN_VAULT_PATH")}/visualizations", "visualization_tool") \
+    plan = (
+        PlanBuilder(
+            f"Create a concept map image from the note with title {note_name}"
+        )
+        .step(
+            "List all available vaults", "mcp:obsidian:list_available_vaults"
+        )
+        .step(
+            f"Fetch the note named '{note_name}' from the obsidian vaults",
+            "mcp:obsidian:read_note",
+        )
+        .step(
+            f"Create a concept map visualization using the extracted relationships. Title the image {note_name} and output the image to the directory {os.getenv("OBSIDIAN_VAULT_PATH")}/visualizations",
+            "visualization_tool",
+        )
         .build()
+    )
     portia.storage.save_plan(plan)
     return plan
+
 
 def create_plan_remote(portia: Portia, note_name: str):
     query = f"""
@@ -32,17 +50,31 @@ def create_plan_remote(portia: Portia, note_name: str):
       - The output directory for the concept map is {os.getenv("OBSIDIAN_VAULT_PATH")}/visualizations.
     """
     return portia.plan(query)
-    
 
-def main():
+
+def main(argv=sys.argv[1:]):
+    load_dotenv()
+
+    if os.getenv("OBSIDIAN_VAULT_PATH") is None:
+        raise ValueError(
+            "OBSIDIAN_VAULT_PATH is not set in your environment variables"
+        )
+
+    argument_parser = argparse.ArgumentParser()
+    argument_parser.add_argument(
+        "note",
+        help="The name of the note to be visualised.",
+        default="Local LLMs",
+    )
+
+    args = argument_parser.parse_args(argv)
+
     config = Config.from_default(
         default_log_level="DEBUG",
         default_model="ollama/qwen3:4b",
         execution_agent_type=ExecutionAgentType.ONE_SHOT,
     )
     # Make sure OBSIDIAN_VAULT_PATH is set in your environment variables
-    if os.getenv("OBSIDIAN_VAULT_PATH") is None:
-        raise ValueError("OBSIDIAN_VAULT_PATH is not set in your environment variables")
 
     obsidian_mcp = McpToolRegistry.from_stdio_connection(
         server_name="obsidian",
@@ -53,10 +85,7 @@ def main():
     visualization_tool = VisualizationTool()
 
     # Add all tools to the registry
-    tools = obsidian_mcp + ToolRegistry([
-        LLMTool(),
-        visualization_tool
-    ])
+    tools = obsidian_mcp + ToolRegistry([LLMTool(), visualization_tool])
 
     portia = Portia(
         config=config,
@@ -64,11 +93,10 @@ def main():
         tools=tools,
     )
 
-    plan = create_plan_local(portia, "Local LLMs")
+    plan = create_plan_local(portia, args.note)
     print(plan.pretty_print())
     portia.run_plan(plan)
 
 
 if __name__ == "__main__":
-    load_dotenv()
     main()
