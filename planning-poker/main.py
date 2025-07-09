a25ab49a4d7c1afb9534d1cae8337581e9af3acb
Momo <mounir@portialabs.ai>
2025-07-09 18:13:37 +0100

    Momo planning poker plus plus (#61)
    * planning poker
    
    * devto changes

--- planning-poker/main.py
+++ planning-poker/main.py
@@ -1,13 +1,30 @@
-from portia import Portia, PlanRunState, PlanBuilder, Config
+from portia import Portia, Config, DefaultToolRegistry, InMemoryToolRegistry
+from portia.cli import CLIExecutionHooks
 from portia.open_source_tools.llm_tool import LLMTool
 from pydantic import BaseModel, Field
 from enum import Enum
 from dotenv import load_dotenv
 
 load_dotenv(override=True)
-config = Config.from_default()
-portia = Portia(config=config)
 
+# Define personas of in the planning poker and load Portia codebase summary to help with estimation
+personas = [
+    "You are a frontend developer, assess the ticket and estimate with your relevant experience",
+    "You are a backend developer, assess the ticket and estimate with your relevant experience",
+    "You are a devops engineer, assess the ticket and estimate with your relevant experience",
+]
+
+with open("./context.md", "r") as f:
+    codebase_context = f.read()
+tool_context = f"Conform to the nearest sizing value, rounding up to the nearest day. If the ticket is too large to estimate, return TLTE.\n\n{codebase_context}"
+
+estimate_tool = LLMTool(
+    id='ticket_estimator_tool',
+    name='Ticket Estimator',
+    tool_context=tool_context,
+)
+
+# Define output schema for Linear tickets and the planning poker estimates
 class Sizing(Enum):
     """The size of the planning poker estimate"""
     ONE_DAY = "1D"
@@ -19,77 +36,39 @@ class Sizing(Enum):
 
 class PlanningPokerEstimate(BaseModel):
     """A planning poker card estimate"""
+    ticket_id: str = Field(..., description="The id of the ticket in Linear")
+    ticket_title: str = Field(..., description="The title of the ticket")
     size: Sizing = Field(..., description="The size of the planning poker card")
     reason: str = Field(..., description="Discuss the reasoning behind the estimate, any issues or assumptions you made, and any other relevant information")
 
+class PlanningPokerEstimateList(BaseModel):
+    """A list of planning poker estimates"""
+    estimates: list[PlanningPokerEstimate] = Field(..., description="The estimates")
 
-class LinearTicket(BaseModel):
-    """A linear ticket"""
-    title: str = Field(..., description="The title of the ticket")
-    description: str = Field(..., description="The description of the ticket")
-    ticket_id: str = Field(..., description="The id of the ticket")
-
-class LinearTicketList(BaseModel):
-    """A list of linear tickets"""
-    tickets: list[LinearTicket] = Field(..., description="The tickets")
-
-class RepoStructure(BaseModel):
-    """A structure of a repo"""
-    name: str = Field(..., description="The name of the repo")
-    pages: list[str] = Field(..., description="The pages of the repo")
-
-for tool in portia.tool_registry:
-    print(tool.id)
-
-ticket_type = "Async Portia"
-query = f"Get the tickets i'm working on from linear regarding {ticket_type}"
-plan = PlanBuilder(
-    query, structured_output_schema=LinearTicketList
-).step(
-    query + " and only call the tool with a limit of 10", tool_id="portia:mcp:mcp.linear.app:list_my_issues"
-).step(
-    f"Filter the tickets to only include the ones related to {ticket_type}", tool_id="llm_tool"
-).build()
-plan_run = portia.run_plan(plan)
-tickets = plan_run.outputs.final_output.value.tickets
-print(tickets)
-
-
-with open("./context.md", "r") as f:
-    codebase_context = f.read()
-
-
-tool_context = f"Conform to the nearest sizing value, rounding up to the nearest day. If the ticket is too large to estimate, return TLTE.\n\n{codebase_context}"
-
-estimate_tool = LLMTool(
-    id='ticket_estimator_tool',
-    name='Ticket Estimator',
+# Initialize Portia
+config = Config.from_default()
+portia = Portia(
+    config=config,
+    tools=DefaultToolRegistry(config=config) + InMemoryToolRegistry([estimate_tool]),
+    execution_hooks=CLIExecutionHooks(),
 )
 
-personas = [
-    "You are a frontend developer, assess the ticket and estimate with your relevant experience",
-    "You are a backend developer, assess the ticket and estimate with your relevant experience",
-    "You are a devops engineer, assess the ticket and estimate with your relevant experience",
-]
+# Get tickets from Linear and estimate the size of the tickets
+project = "Async Portia"
+query = f"""Get the tickets i'm working on from Linear with a limit of 3 on the tool call. Then filter specifically for those regarding the {project} project.
+    For each combination of the tickets above and the following personas, estimate the size of the ticket.
+    {personas}
+    
+    Return the estimates in a list of PlanningPokerEstimate objects, with estimate sizes averaged across the personas for each ticket.
+    """
+estimates = portia.run(
+    query=query,
+    structured_output_schema=PlanningPokerEstimateList,
+).outputs.final_output.value.estimates # type: ignore[attr-defined]
 
-for ticket in tickets:
-    estimates = []
-    estimate_plan = PlanBuilder(
-        "estimate the size of the ticket", structured_output_schema=PlanningPokerEstimate
-    ).step(f"Estimate the size of the ticket: {ticket.title}\n\n{ticket.description}", tool_id="ticket_estimator_tool").build()
-    for persona in personas:
-        context = f"""
-        {persona}
-        {tool_context}
-        """
-        estimate_tool.tool_context = context
-        portia.tool_registry.with_tool(estimate_tool, overwrite=True)
-        estimate = portia.run_plan(estimate_plan)
-        if estimate.state == PlanRunState.COMPLETE:
-            estimates.append(estimate.outputs.final_output.value)
-
-    for estimate in estimates:
-        print(f"Ticket: {ticket.title} - Estimate: {estimate.size} -\nReason: {estimate.reason}")
-
-
-
+# Print the estimates
+print(f"\n\n***** Printing {len(estimates)} estimates *****\n")
+for estimate in estimates:
+    print(f"""Ticket: {estimate.ticket_id} - {estimate.ticket_title} - Estimate: {estimate.size} 
+        Reason: {estimate.reason}"""
+    )
